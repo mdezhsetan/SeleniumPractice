629976bd723888fdd3edad13e45677d2733998d6
Mahshad <mahshad.dezhsetan@gmail.com>
2022-08-15 20:49:45 +0400

    chrome driver configured

--- src/main/java/org/selenium4/Main.java
+++ src/main/java/org/selenium4/Main.java
@@ -1,7 +1,19 @@
 package org.selenium4;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        System.out.println("\n\n\n***************************************\n\nHello world!");
+
+        driver.close();
+        driver.quit();
     }
 }

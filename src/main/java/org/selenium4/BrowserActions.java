346817241d46cbb1da96410541542ddcf5eb29be
Mahshad <mahshad.dezhsetan@gmail.com>
2022-08-17 14:32:02 +0400

    BrowserActions: changing window size and position, also taking screenshots

--- src/main/java/org/selenium4/BrowserActions.java
+++ src/main/java/org/selenium4/BrowserActions.java
@@ -1,10 +1,14 @@
 package org.selenium4;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class BrowserActions {
@@ -50,5 +54,54 @@ public class BrowserActions {
 
     public void tearDown() {
         driver.quit();
+    }
+
+
+    public void frameSelection() {
+        driver.navigate().to("https://www.play1.automationcamp.ir/");
+        driver.findElement(By.cssSelector("[href='frames.html']")).click();
+    }
+
+    public void windowManagement() throws InterruptedException {
+        System.out.println(driver.manage().window().getSize().height);
+        System.out.println(driver.manage().window().getSize().width);
+
+        Dimension size = driver.manage().window().getSize();
+        System.out.println("Size= " + size);
+
+        driver.manage().window().setSize(new Dimension(110, 140));
+
+        System.out.println(driver.manage().window().getPosition().x);
+        System.out.println(driver.manage().window().getPosition().y);
+
+        Point position = driver.manage().window().getPosition();
+        System.out.println("Position = " + position);
+
+        driver.manage().window().setPosition(new Point(250, 250));
+        driver.manage().window().setSize(new Dimension(800, 800));
+        Thread.sleep(1000);
+
+        driver.manage().window().fullscreen();
+        Thread.sleep(1000);
+
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+        driver.manage().window().minimize();
+    }
+
+    public void takeScreenshot() throws IOException {
+
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile, new File("./testScreenshots/ss.png"));
+/*
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.elementToBeClickable(By.id("click_me_2")));
+*/
+        driver.switchTo().frame("frame1").switchTo().frame("frame2");
+        WebElement webElement = driver.findElement(By.cssSelector("#click_me_2"));
+        File srcFile2 = webElement.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile2, new File("./testScreenshots/ss2.png"));
     }
 }
--- src/main/java/org/selenium4/Main.java
+++ src/main/java/org/selenium4/Main.java
@@ -1,11 +1,16 @@
 package org.selenium4;
 
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BrowserActions ba = new BrowserActions();
         ba.setUp();
-        ba.changeUrl();
+        // ba.changeUrl();
+        ba.frameSelection();
+        // ba.windowManagement();
+        ba.takeScreenshot();
     }
 }
